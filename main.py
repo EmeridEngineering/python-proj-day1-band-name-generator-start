be53314565f101cf77186e22c507057a2f2c86af
EmeridEngineering <150422562+EmeridEngineering@users.noreply.github.com>
2024-06-13 10:54:52 +0000

    add a message explaining it is a proposed brand name

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ city_name = input("What's the name of the city you grew up in?\n")
 pet_name = input("What's the name of your pet?\n")
 #4. Combine the name of their city and pet and show them their band name.
 band_name = city_name + " " + pet_name
-print(band_name)
+print("Your band name could be " + band_name)
 #5. Make sure the input cursor shows on a new line:
 
 # Solution: https://replit.com/@appbrewery/band-name-generator-end
